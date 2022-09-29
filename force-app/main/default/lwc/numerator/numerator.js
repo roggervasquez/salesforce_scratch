2756b98259ad1724e2de36ab7ab9d4a2f18aeed5
Rogger Vásquez <rogger.vasquez@gmail.com>
2022-09-29 14:47:34 -0600

    Parent to child passing data

--- force-app/main/default/lwc/numerator/numerator.js
+++ force-app/main/default/lwc/numerator/numerator.js
@@ -1,8 +1,8 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
 export default class Numerator extends LightningElement {
 
-    counter = 0;
+    @api counter = 0;
     handleIncrement() {
       this.counter++;
     }
