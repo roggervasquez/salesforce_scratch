4216afcaf53fa72279301b26ba58e81caba35eb9
Rogger Vásquez <rogger.vasquez@gmail.com>
2022-09-29 18:23:45 -0600

    Setting the counter as a property, getter and setter

--- force-app/main/default/lwc/numerator/numerator.js
+++ force-app/main/default/lwc/numerator/numerator.js
@@ -2,7 +2,18 @@ import { LightningElement, api } from 'lwc';
 
 export default class Numerator extends LightningElement {
 
-    @api counter = 0;
+   // @api counter = 0;
+   _currentCount = 0;
+   priorCount = 0;
+   @api
+   get counter() {
+     return this._currentCount;
+   }
+   set counter(value) {
+     this.priorCount = this._currentCount;
+     this._currentCount = value;
+   }
+   
     handleIncrement() {
       this.counter++;
     }
